2fb9b1e0f50ae7112757841430e459e63fe1c8d4
napaholic <jaewkim@student.42seoul.kr>
2022-07-16 13:58:55 +0900

    iterator.hpp add category

--- myContainer/inc/ft_iterator.hpp
+++ myContainer/inc/ft_iterator.hpp
@@ -9,6 +9,12 @@
 
 namespace ft {
 	
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+	
 	template<class _Category,
 			class _T,
 			class _Distance = ptrdiff_t,
