94712439d0e104c17565aa984b5a381e5f52bb92
Jaewook Kim <jaewkim@c1r8s7.42seoul.kr>
2022-07-23 19:52:29 +0900

    vector code start

--- myContainer/inc/ft_vector.hpp
+++ myContainer/inc/ft_vector.hpp
@@ -12,8 +12,43 @@ namespace ft {
 	template <class T, class Allocator = std::allocator<T> >
 	class vector {
 	public:
+		//Member types===========================================================================
 		typedef T													value_type;
-		typedef Allocator
+		typedef Allocator											allocator_type;
+		typedef typename allocator_type::reference					reference;
+		typedef typename allocator_type::const_reference				const_reference;
+		typedef typename allocator_type::pointer						pointer;
+		typedef typename allocator_type::const_pointer					const_pointer;
+		typedef typename allocator_type::size_type						size_type;
+		typedef ft::random_access_iterator<value_type>					iterator;
+		typedef ft::random_access_iterator<const value_type>			const_iterator;
+		typedef ft::reverse_iterator<iterator>							reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>					const_reverse_iterator;
+		typedef typename ft::iterator_traits<iterator>::difference_type	difference_type;
+
+	protected:
+		//Member variables=======================================================================
+		pointer 		_start;
+		pointer 		_end;
+		pointer			_capacity;
+		allocator_type	_alloc;
+
+	public:
+		//Constructors===========================================================================
+
+		explicit vector(const allocator_type& alloc = allocator_type())
+			: _start(ft_nullptr), _end(ft_nullptr), _capacity(ft_nullptr), _alloc(alloc)
+		{}
+
+		explicit vector(size_type n, const value_type& value = value_type(), const allocator_type& alloc = allocator_type())
+			: _start(ft_nullptr), _end(ft_nullptr), _capacity(ft_nullptr), _alloc(alloc)
+		{
+			_start = _alloc.allocate(n);
+			_end = _start;
+			_capacity = _start + n;
+			for (size_type i = 0; i < n; i++)
+				_alloc.construct(_end++, value);
+		}
 	};
 }
 
--- reference/jaemoojung/map.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __MAP_H__
-#define __MAP_H__
-
-#endif // __MAP_H__
