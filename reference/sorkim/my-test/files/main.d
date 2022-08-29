382ca208a674b1c5641c3a64d1e8117f6e1c8849
napaholic <jaewkim@student.42seoul.kr>
2022-08-29 18:30:01 +0900

    fix ref

---
+++ reference/sorkim/my-test/files/main.d
@@ -0,0 +1,23 @@
+files/main.o: main.cpp ../vector.hpp ../reverseIterator.hpp \
+  ../iterator.hpp ../utils.hpp ../map.hpp ../tree.hpp vector_tests.cpp \
+  stack_test.cpp ../stack.hpp map_tests.cpp
+
+../vector.hpp:
+
+../reverseIterator.hpp:
+
+../iterator.hpp:
+
+../utils.hpp:
+
+../map.hpp:
+
+../tree.hpp:
+
+vector_tests.cpp:
+
+stack_test.cpp:
+
+../stack.hpp:
+
+map_tests.cpp:
